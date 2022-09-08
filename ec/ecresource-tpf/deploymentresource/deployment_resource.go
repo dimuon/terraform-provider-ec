45bfccf59357404cd387c36cacdd78e0ffb6940c
Dmytro Onishchenko <dmytro.onishchenko@elastic.co>
2022-09-08 17:20:17 +0200

    WIP: deploymentresource schema

--- ec/ecresource-tpf/deploymentresource/deployment_resource.go
+++ ec/ecresource-tpf/deploymentresource/deployment_resource.go
@@ -21,6 +21,7 @@ import (
 	"context"
 
 	"github.com/elastic/terraform-provider-ec/ec/internal"
+	"github.com/elastic/terraform-provider-ec/ec/internal/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	tpfprovider "github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -88,16 +89,6 @@ func (t DeploymentResourceType) GetSchema(ctx context.Context) (tfsdk.Schema, di
 				Computed:  true,
 				Sensitive: true,
 			},
-			// "elasticsearch": {
-			// 	Type:        types.ListType{
-			// 		ElemType: ,
-			// 	},
-			// 	Description: "Required Elasticsearch resource definition",
-			// 	MaxItems:    1,
-			// 	Required:    true,
-			// 	Elem:        newElasticsearchResource(),
-			// },
-
 		},
 
 		Blocks: map[string]tfsdk.Block{
@@ -106,20 +97,19 @@ func (t DeploymentResourceType) GetSchema(ctx context.Context) (tfsdk.Schema, di
 				MaxItems:    1,
 				Attributes: map[string]tfsdk.Attribute{
 					"autoscale": {
-						Type:        types.StringType,
+						Type:        types.BoolType,
 						Description: `Enable or disable autoscaling. Defaults to the setting coming from the deployment template. Accepted values are "true" or "false".`,
 						Computed:    true,
 						Optional:    true,
-						// ValidateFunc: func(i interface{}, s string) ([]string, []error) {
-						// 	if _, err := strconv.ParseBool(i.(string)); err != nil {
-						// 		return nil, []error{
-						// 			fmt.Errorf("failed parsing autoscale value: %w", err),
-						// 		}
-						// 	}
-						// 	return nil, nil
+					},
+					"ref_id": {
+						Type:        types.StringType,
+						Description: "Optional ref_id to set on the Elasticsearch resource",
+						Optional:    true,
+						// PlanModifiers: []tfsdk.AttributePlanModifier{
+						// 	planmodifier.DefaultValue(types.String{Value: "main-elasticsearch"}),
 						// },
 					},
-
 					"resource_id": {
 						Type:        types.StringType,
 						Description: "The Elasticsearch resource unique identifier",
@@ -128,6 +118,16 @@ func (t DeploymentResourceType) GetSchema(ctx context.Context) (tfsdk.Schema, di
 					"region": {
 						Type:        types.StringType,
 						Description: "The Elasticsearch resource region",
+						Computed:    true,
+					},
+					"cloud_id": {
+						Type:        types.StringType,
+						Description: "The encoded Elasticsearch credentials to use in Beats or Logstash",
+						Computed:    true,
+					},
+					"http_endpoint": {
+						Type:        types.StringType,
+						Description: "The Elasticsearch resource HTTP endpoint",
 						Computed:    true,
 					},
 					"https_endpoint": {
@@ -141,11 +141,145 @@ func (t DeploymentResourceType) GetSchema(ctx context.Context) (tfsdk.Schema, di
 					"topology": {
 						NestingMode: tfsdk.BlockNestingModeList,
 						MinItems:    0,
+						Description: `Optional topology element which must be set once but can be set multiple times to compose complex topologies`,
 						Attributes: map[string]tfsdk.Attribute{
 							"id": {
 								Type:        types.StringType,
 								Description: `Required topology ID from the deployment template`,
 								Required:    true,
+							},
+							"instance_configuration_id": {
+								Type:        types.StringType,
+								Description: `Computed Instance Configuration ID of the topology element`,
+								Computed:    true,
+							},
+							"size": {
+								Type:        types.StringType,
+								Description: `Optional amount of memory per node in the "<size in GB>g" notation`,
+								Computed:    true,
+								Optional:    true,
+							},
+							"size_resource": {
+								Type:        types.StringType,
+								Description: `Optional size type, defaults to "memory".`,
+								Optional:    true,
+								Computed:    true,
+								PlanModifiers: []tfsdk.AttributePlanModifier{
+									planmodifier.DefaultValue(types.String{Value: "memory"}),
+								},
+							},
+							"zone_count": {
+								Type:        types.StringType,
+								Description: `Optional number of zones that the Elasticsearch cluster will span. This is used to set HA`,
+								Computed:    true,
+								Optional:    true,
+							},
+							"node_type_data": {
+								Type:        types.StringType,
+								Description: `The node type for the Elasticsearch Topology element (data node)`,
+								Computed:    true,
+								Optional:    true,
+							},
+							"node_type_master": {
+								Type:        types.StringType,
+								Description: `The node type for the Elasticsearch Topology element (master node)`,
+								Computed:    true,
+								Optional:    true,
+							},
+							"node_type_ingest": {
+								Type:        types.StringType,
+								Description: `The node type for the Elasticsearch Topology element (ingest node)`,
+								Computed:    true,
+								Optional:    true,
+							},
+							"node_type_ml": {
+								Type:        types.StringType,
+								Description: `The node type for the Elasticsearch Topology element (machine learning node)`,
+								Computed:    true,
+								Optional:    true,
+							},
+							"node_roles": {
+								Type: types.SetType{
+									ElemType: types.StringType,
+								},
+								Description: `The computed list of node roles for the current topology element`,
+								Computed:    true,
+							},
+						},
+
+						Blocks: map[string]tfsdk.Block{
+							"autoscaling": {
+								NestingMode: tfsdk.BlockNestingModeList,
+								MinItems:    0,
+								MaxItems:    1,
+								Description: "Optional Elasticsearch autoscaling settings, such a maximum and minimum size and resources.",
+								Attributes: map[string]tfsdk.Attribute{
+									"max_size_resource": {
+										Description: "Maximum resource type for the maximum autoscaling setting.",
+										Type:        types.StringType,
+										Optional:    true,
+										Computed:    true,
+									},
+									"max_size": {
+										Description: "Maximum size value for the maximum autoscaling setting.",
+										Type:        types.StringType,
+										Optional:    true,
+										Computed:    true,
+									},
+									"min_size_resource": {
+										Description: "Minimum resource type for the minimum autoscaling setting.",
+										Type:        types.StringType,
+										Optional:    true,
+										Computed:    true,
+									},
+									"min_size": {
+										Description: "Minimum size value for the minimum autoscaling setting.",
+										Type:        types.StringType,
+										Optional:    true,
+										Computed:    true,
+									},
+									"policy_override_json": {
+										Type:        types.StringType,
+										Description: "Computed policy overrides set directly via the API or other clients.",
+										Computed:    true,
+									},
+								},
+							},
+
+							"config": {
+								NestingMode: tfsdk.BlockNestingModeList,
+								MinItems:    0,
+								MaxItems:    1,
+								Description: `Computed read-only configuration to avoid unsetting plan settings from 'topology.elasticsearch'`,
+								Attributes: map[string]tfsdk.Attribute{
+									"plugins": {
+										Type: types.SetType{
+											ElemType: types.StringType,
+										},
+										Description: "List of Elasticsearch supported plugins, which vary from version to version. Check the Stack Pack version to see which plugins are supported for each version. This is currently only available from the UI and [ecctl](https://www.elastic.co/guide/en/ecctl/master/ecctl_stack_list.html)",
+										Computed:    true,
+									},
+									"user_settings_json": {
+										Type:        types.StringType,
+										Description: `JSON-formatted user level "elasticsearch.yml" setting overrides`,
+										Computed:    true,
+									},
+									"user_settings_override_json": {
+										Type:        types.StringType,
+										Description: `JSON-formatted admin (ECE) level "elasticsearch.yml" setting overrides`,
+										Computed:    true,
+									},
+									"user_settings_yaml": {
+										Type:        types.StringType,
+										Description: `YAML-formatted user level "elasticsearch.yml" setting overrides`,
+										Computed:    true,
+									},
+									"user_settings_override_yaml": {
+										Type:        types.StringType,
+										Description: `YAML-formatted admin (ECE) level "elasticsearch.yml" setting overrides`,
+										Computed:    true,
+									},
+								},
 							},
 						},
 					},
