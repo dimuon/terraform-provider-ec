1ad5058fc2e4c5d7181d992aa153a3c0478c9a26
Dmytro Onishchenko <dmytro.onishchenko@elastic.co>
2022-10-11 17:28:15 +0200

    delete obsolete file

--- ec/ecresource-tpf/deploymentresource/testdata/deployment-with-defaults-io-optimized.tf
+++
@@ -1,7 +0,0 @@
-resource "ec_deployment" "empty declarations (IO Optimized)" {
-    name = "my_deployment_name"
-    deployment_template_id = "aws-io-optimized-v2"
-    region = "us-east-1"
-    version = "7.7.0"
-    traffic_filter = ["0.0.0.0/0", "192.168.10.0/24"]
-}
