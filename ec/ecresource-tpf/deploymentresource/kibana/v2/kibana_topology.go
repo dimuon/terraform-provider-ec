3185be20584284926da7611569d5181d6f2caffc
Dmytro Onishchenko <dmytro.onishchenko@elastic.co>
2022-12-02 12:09:03 +0100

    add missing file

---
+++ ec/ecresource-tpf/deploymentresource/kibana/v2/kibana_topology.go
@@ -0,0 +1,88 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package v2
+
+import (
+	"github.com/elastic/cloud-sdk-go/pkg/models"
+	"github.com/elastic/cloud-sdk-go/pkg/util/ec"
+	topologyv1 "github.com/elastic/terraform-provider-ec/ec/ecresource-tpf/deploymentresource/topology/v1"
+	"github.com/elastic/terraform-provider-ec/ec/internal/util"
+
+	"github.com/elastic/terraform-provider-ec/ec/ecresource-tpf/deploymentresource/utils"
+)
+
+const (
+	minimumKibanaSize = 1024
+)
+
+func readKibanaTopology(in *models.KibanaClusterTopologyElement) (*topologyv1.Topology, error) {
+	var top topologyv1.Topology
+
+	if in.InstanceConfigurationID != "" {
+		top.InstanceConfigurationId = &in.InstanceConfigurationID
+	}
+
+	if in.Size != nil {
+		top.Size = ec.String(util.MemoryToState(*in.Size.Value))
+		top.SizeResource = ec.String(*in.Size.Resource)
+	}
+
+	top.ZoneCount = int(in.ZoneCount)
+
+	return &top, nil
+}
+
+type KibanaTopologiesTF []*topologyv1.TopologyTF
+
+func readKibanaTopologies(in []*models.KibanaClusterTopologyElement) (topologyv1.Topologies, error) {
+	if len(in) == 0 {
+		return nil, nil
+	}
+
+	tops := make(topologyv1.Topologies, 0, len(in))
+	for _, model := range in {
+		if model.Size == nil || model.Size.Value == nil || *model.Size.Value == 0 {
+			continue
+		}
+
+		top, err := readKibanaTopology(model)
+		if err != nil {
+			return nil, err
+		}
+
+		tops = append(tops, *top)
+	}
+
+	return tops, nil
+}
+
+// defaultKibnaTopology iterates over all the templated topology elements and
+// sets the size to the default when the template size is greater than the
+// local terraform default, the same is done on the ZoneCount.
+func defaultKibanaTopology(topology []*models.KibanaClusterTopologyElement) []*models.KibanaClusterTopologyElement {
+	for _, t := range topology {
+		if *t.Size.Value > minimumKibanaSize {
+			t.Size.Value = ec.Int32(minimumKibanaSize)
+		}
+		if t.ZoneCount > utils.MinimumZoneCount {
+			t.ZoneCount = utils.MinimumZoneCount
+		}
+	}
+
+	return topology
+}
